a7f5010d1388f6ce92fd834d2208fb0045819ba9
Or <orharush24@gmail.com>
2025-10-05 02:09:00 +0300

    client deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -11,6 +12,11 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'assets'),
+    },
+  },
   server: {
     port: 3000,
     open: true,
